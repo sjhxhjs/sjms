be6348b9a08e54a6c4b88270d891ece370abce71
byebyebird <763152431@qq.com>
2019-04-29 22:39:15 +0800

    单例模式

---
+++ singleton/src/SingleTon.java
@@ -0,0 +1,13 @@
+/**
+ * 单例模式
+ */
+public class SingleTon {
+    private static final SingleTon singleton = new SingleTon();
+    private SingleTon() {
+
+    }
+
+    public static SingleTon getSingleton() {
+        return singleton;
+    }
+}
